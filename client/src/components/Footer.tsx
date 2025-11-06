8b122bf260bbb6410bb7f2e3002b2b3feb0b7b1c
Maryam597 <maryam.dri@hotmail.fr>
2025-11-06 16:27:24 +0100

    contact css

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -7,21 +7,21 @@ const Footer = () => (
       <ul>
         <h2>Liens Utiles</h2>
         <li><a href="https://www.linkedin.com/in/maryam-dri/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-        <li><a href="https://github.com/maryam-dri" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+        <li><a href="https://github.com/maryam597" target="_blank" rel="noopener noreferrer">GitHub</a></li>
         <li><a href="mailto:maryam.dri@hotmail.fr">Envoyer un Email</a></li>
       </ul>
 
       <ul>
         <h2>Pages</h2>
-        <li><a href="#home">Accueil</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#projects">Projets</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/projects">Projets</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
 
       <ul>
         <h2>Informations</h2>
-        <li>📍 Villeneuve d’Ascq</li>
+        <li>📍 Lille, France</li>
         <li>📞 07.50.47.46.23</li>
         <li>📧 maryam.dri@hotmail.fr</li>
       </ul>
