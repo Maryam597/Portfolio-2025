cec18c0475d70fd919ac7360aec1a3b8920f331f
Maryam597 <maryam.dri@hotmail.fr>
2025-11-07 17:37:57 +0100

    homepage css

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => (
         <h2>Liens Utiles</h2>
         <li><a href="https://www.linkedin.com/in/maryam-dri/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={20} color="#0A66C2" />LinkedIn </a></li>
         <li><a href="https://github.com/maryam597" target="_blank" rel="noopener noreferrer"><FaGithub size={20} color="#e3e8edff" /> GitHub</a></li>
-        <li><a href="mailto:maryam.dri@hotmail.fr"><SiMailboxdotorg size={20} color="#0A66C2" />Envoyer un Email</a></li>
+        <li><a href="mailto:maryam.dri@hotmail.fr"><SiMailboxdotorg size={20} color="#ea5873ff" />Envoyer un Email</a></li>
       </ul>
 
       <ul>
