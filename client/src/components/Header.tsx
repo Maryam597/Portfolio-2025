156f822b5e5ee233af4ba72723f3c940ad3efe00
Maryam597 <maryam.dri@hotmail.fr>
2025-11-13 22:15:23 +0100

    translation

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,12 +5,10 @@ const Header = () => (
   <header className={styles.header}>
     <nav className={styles.nav}>
       <ul className={styles.ul}>
-        <li className={styles.li}><Link to="/">Home</Link></li>
+        <li className={styles.li}><Link to="/">Accueil</Link></li>
         <li className={styles.li}><Link to="/services">Services</Link></li>
-        <li className={styles.li}><Link to="/projects">Projects</Link></li>
+        <li className={styles.li}><Link to="/projects">Projets</Link></li>
         <li className={styles.li}><Link to="/contact">Contact</Link></li>
-        {/* <li className={styles.li}><Link to="/admin/login">Admin Login</Link></li>
-        <li className={styles.li}><Link to="/admin/dashboard">Admin Dashboard</Link></li> */}
       </ul>
     </nav>
   </header>
