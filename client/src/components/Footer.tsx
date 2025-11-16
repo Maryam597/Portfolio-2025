4c381b0ca4b004714b4c2d26086c5c42823a4db9
Maryam597 <maryam.dri@hotmail.fr>
2025-11-16 19:20:34 +0100

    translation - Home ok

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,38 +1,66 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import styles from './Footer.module.css';
-import { SiLinkedin, SiMailboxdotorg, SiGithub} from "react-icons/si";
+import { SiMailboxdotorg } from "react-icons/si";
+import { useTranslation } from "react-i18next";
 
+const Footer = () => {
+  const { t } = useTranslation();
 
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <div className={styles.footerContent}>
+        <ul>
+          <h2>{t("footer.usefulLinks.title")}</h2>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/maryam-dri/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={20} color="#0A66C2" />
+              {t("footer.usefulLinks.linkedin")}
+            </a>
+          </li>
 
-      <ul>
-        <h2>Liens Utiles</h2>
-        <li><a href="https://www.linkedin.com/in/maryam-dri/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={20} color="#0A66C2" />LinkedIn </a></li>
-        <li><a href="https://github.com/maryam597" target="_blank" rel="noopener noreferrer"><FaGithub size={20} color="#e3e8edff" /> GitHub</a></li>
-        <li><a href="mailto:maryam.dri@hotmail.fr"><SiMailboxdotorg size={20} color="#ea5873ff" />Envoyer un Email</a></li>
-      </ul>
+          <li>
+            <a
+              href="https://github.com/maryam597"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={20} color="#e3e8edff" />
+              {t("footer.usefulLinks.github")}
+            </a>
+          </li>
 
-      <ul>
-        <h2>Pages</h2>
-        <li><a href="/">Accueil</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/projects">Projets</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
+          <li>
+            <a href="mailto:maryam.dri@hotmail.fr">
+              <SiMailboxdotorg size={20} color="#ea5873ff" />
+              {t("footer.usefulLinks.email")}
+            </a>
+          </li>
+        </ul>
 
-      <ul>
-        <h2>Informations</h2>
-        <li>📍 Lille, France</li>
-        <li>📞 07.50.47.46.23</li>
-        <li>📧 maryam.dri@hotmail.fr</li>
-      </ul>
-    </div>
-    <p>© 2025 Maryam Portfolio</p>
+        <ul>
+          <h2>{t("footer.pages.title")}</h2>
+          <li><a href="/">{t("footer.pages.home")}</a></li>
+          <li><a href="/services">{t("footer.pages.services")}</a></li>
+          <li><a href="/projects">{t("footer.pages.projects")}</a></li>
+          <li><a href="/contact">{t("footer.pages.contact")}</a></li>
+        </ul>
 
-  </footer>
-);
+        <ul>
+          <h2>{t("footer.informations.title")}</h2>
+          <li>📍 {t("footer.informations.location")}</li>
+          <li>📞 {t("footer.informations.phone")}</li>
+          <li>📧 {t("footer.informations.email")}</li>
+        </ul>
+      </div>
+
+      <p>© 2025 Maryam Portfolio</p>
+    </footer>
+  );
+};
 
 export default Footer;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,22 @@
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const Header = () => (
-  <header className={styles.header}>
-    <nav className={styles.nav}>
-      <ul className={styles.ul}>
-        <li className={styles.li}><Link to="/">Accueil</Link></li>
-        <li className={styles.li}><Link to="/services">Services</Link></li>
-        <li className={styles.li}><Link to="/projects">Projets</Link></li>
-        <li className={styles.li}><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const { t } = useTranslation();
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          <li className={styles.li}><Link to="/">{t("header.home")}</Link></li>
+          <li className={styles.li}><Link to="/services">{t("header.services")}</Link></li>
+          <li className={styles.li}><Link to="/projects">{t("header.projects")}</Link></li>
+          <li className={styles.li}><Link to="/contact">{t("header.contact")}</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
